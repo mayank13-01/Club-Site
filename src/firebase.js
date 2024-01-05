254b30e86db3e6a0e26beb4787b46ad1b447f2e8
Mayank Sharma <92167883+mayank13-01@users.noreply.github.com>
2024-01-05 19:41:07 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwcX_cGiPwnQEtAI8zJjLILCbGkXjL8o4",
-  authDomain: "majorproject-2f5f2.firebaseapp.com",
-  databaseURL: "https://majorproject-2f5f2-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "majorproject-2f5f2",
-  storageBucket: "majorproject-2f5f2.appspot.com",
-  messagingSenderId: "756841882675",
-  appId: "1:756841882675:web:248376d8ec3dbc0ff3ef46",
-  measurementId: "G-JLR5NX2QXG"
+  //USE YOUR FIREBASE CONFIG
 };
 
 // Initialize Firebase
